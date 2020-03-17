3ef952422b32f816ef860e5bee5515eade50978d
Igor <workuglis@gmail.com>
2020-03-17 17:12:25 +0200

    3.2. Фиксация изменений на github[#257470]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Igor Noroc");
+        System.out.println("17.02.1986");
+    }
+}
