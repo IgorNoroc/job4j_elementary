8d3c82bac2c11f9c3f8bd81374484456115fc2c2
Igor <workuglis@gmail.com>
2020-03-19 14:30:44 +0200

    4.5. Отладка программы в IDEA[#257406]

--- src/ru/job4j/condition/SqAreaTest.java
+++ src/ru/job4j/condition/SqAreaTest.java
@@ -7,9 +7,9 @@ public class SqAreaTest {
 
     @Test
     public void square() {
-        int in1 = 6;
-        int in2 = 2;
-        double out = 2.0;
+        int in1 = 4;
+        int in2 = 1;
+        double out = 1.0;
         double expected = SqArea.square(in1, in2);
         Assert.assertEquals(expected, out, 0.01);
     }
