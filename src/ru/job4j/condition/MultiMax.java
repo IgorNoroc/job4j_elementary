cabe4b4bb6517b00b77520a7e515351f043b24e2
Igor <workuglis@gmail.com>
2020-03-19 19:49:17 +0200

    5.7. Ипотека.[#257485]

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -1,11 +1,13 @@
 package ru.job4j.condition;
 
 public class MultiMax {
-    public int max(int first, int second, int third) {
-        if (first > second) {
-            return first > third ? first : third;
-        } else {
-            return second > third ? second : third;
+    public static int max(int first, int second, int third) {
+        int result = first;
+        if (second > first && second > third) {
+            result = second;
+        } else if (third > first && third > second) {
+            result = third;
         }
+        return result;
     }
 }
