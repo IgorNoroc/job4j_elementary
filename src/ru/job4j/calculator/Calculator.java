966fd5976271fdcfb6faa183cce9529f38d80c3f
Igor <workuglis@gmail.com>
2020-03-20 14:47:46 +0200

    5.8. JavaDoc. Code convention[#257493]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculator;
 
+/**
+ * Addition of two numbers.
+ *
+ * @author Igor Noroc (workuglis@gmail.com)
+ */
 public class Calculator {
+    /**
+     * Main.
+     * @param args - args.
+     */
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
---
+++ src/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+ /**
+  * Package for calculate task.
+  *
+  * @author Igor Noroc (workuglis@gmail.com)
+  * @version 1.0
+  * @since 19.03.2020
+  */
+ package ru.job4j.calculator;
