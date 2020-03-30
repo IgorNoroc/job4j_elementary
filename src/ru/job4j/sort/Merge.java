d101f89c558d239ee67150c0240a4f62f1331e77
Igor <workuglis@gmail.com>
2020-03-30 13:55:30 +0300

    1. Экзамен. Базовый синтаксис.[#257478]

--- src/ru/job4j/sort/Merge.java
+++ src/ru/job4j/sort/Merge.java
@@ -7,52 +7,25 @@ public class Merge {
     public static void main(String[] args) throws Exception {
         Merge merge = new Merge();
         int[] result = merge.merge(
-                new int[]{9, 31, 32, 35, 100, 233, 1233},
+                new int[]{9, 31, 32, 35, 100, 233, 1233, 6000},
                 new int[]{5, 5, 12, 4000}
         );
         System.out.println(Arrays.toString(result));
     }
 
     public int[] merge(int[] left, int[] right) {
-
-        int[] result = new int[left.length + right.length];
-
-        if (left.length == 0) {
-            result = right;
-        } else if (right.length == 0) {
-            result = left;
-        }
-
         int countL = 0;
         int countR = 0;
         int countOfResult = 0;
 
+        int[] result = new int[left.length + right.length];
         while (countOfResult < result.length) {
-
             if (countL < left.length && countR < right.length) {
-                result[countOfResult] = Math.min(left[countL], right[countR]);
-
-                if (result[countOfResult] == left[countL] && result[countOfResult] == right[countR]) {
-                    result[countOfResult + 1] = left[countL];
-                    countL++;
-                    countR++;
-                    countOfResult++;
-                } else if (result[countOfResult] == left[countL]) {
-                    countL++;
-                } else if (result[countOfResult] == right[countR]) {
-                    countR++;
-                }
-                countOfResult++;
-
+                result[countOfResult++] = left[countL] < right[countR] ? left[countL++] : right[countR++];
             } else if (countL == left.length && countR < right.length) {
-                result[countOfResult] = right[countR];
-                countR++;
-                countOfResult++;
-
+                result[countOfResult++] = right[countR++];
             } else if (countR == right.length && countL < left.length) {
-                result[countOfResult] = left[countL];
-                countL++;
-                countOfResult++;
+                result[countOfResult++] = left[countL++];
             }
         }
         return result;
