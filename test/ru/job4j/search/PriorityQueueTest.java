0b9eef9e38c2a73aeb671b5c44d51cfdaf57934a
Igor <workuglis@gmail.com>
2020-04-12 15:31:05 +0300

    2. Очередь с приоритетом на LinkedList[#257517]

--- test/ru/job4j/search/PriorityQueueTest.java
+++ test/ru/job4j/search/PriorityQueueTest.java
@@ -22,4 +22,21 @@ public class PriorityQueueTest {
         Task result = queue.take();
         assertThat(result.getDesc(), is("one"));
     }
+
+    @Test
+    public void whenUrgentIsLast() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low1", 5));
+        queue.put(new Task("low2", 5));
+        queue.put(new Task("low3", 5));
+        queue.put(new Task("low4", 5));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("middle", 3));
+        queue.put(new Task("urgen", 1));
+        queue.put(new Task("low5", 5));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("urgen"));
+        Task rsl = queue.take();
+        assertThat(rsl.getDesc(), is("urgent"));
+    }
 }
