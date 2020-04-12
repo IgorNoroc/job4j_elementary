f9e1a5e99b18bd0c127b3813ce00786415d7b359
Igor <workuglis@gmail.com>
2020-04-12 16:14:09 +0300

    3. Модели данных и HashSet.[#257531]

---
+++ test/ru/job4j/colection/NotifyAccountTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.colection;
+
+import org.junit.Test;
+import ru.job4j.collection.Account;
+import ru.job4j.collection.NotifyAccount;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class NotifyAccountTest {
+    @Test
+    public void sent() {
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("142", "Petr Arsentev", "000001")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Petr Arsentev", "eDer3432f"),
+                        new Account("142", "Petr Arsentev", "000001")
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+
+    @Test
+    public void when3Eq() {
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Igor", "11100k"),
+                new Account("123", "Igor", "11100k"),
+                new Account("123", "Igor", "11100k")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Igor", "11100k")
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+}
