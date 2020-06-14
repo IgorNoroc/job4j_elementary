ad8a0edf36f66f6abf96e629ade46270fa02c0ba
Igor <workuglis@gmail.com>
2020-06-14 15:39:29 +0300

    изменение файла README.md

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # job4j
 Проект содержит решения блока "Базовый синтаксис" курса Job4j.
-[![Build Status](https://travis-ci.org/uglis/job4j_elementary.svg?branch=master)](https://travis-ci.org/uglis/job4j_elementary)
-
+[![Build Status](https://travis-ci.org/IgorNoroc/job4j_elementary.svg?branch=master)](https://travis-ci.org/IgorNoroc/job4j_elementary)
 [![codecov](https://codecov.io/gh/uglis/job4j_elementary/branch/master/graph/badge.svg)](https://codecov.io/gh/uglis/job4j_elementary)
--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -1,19 +1,36 @@
 package ru.job4j.collection;
 
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.function.Function;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Проверить две строчки на идентичность.
  */
 public class FreezeStr {
-    public static boolean eq(String left, String right) {
-        char[] first = left.toCharArray();
-        char[] second = right.toCharArray();
-        Arrays.sort(first);
-        Arrays.sort(second);
-        return Arrays.equals(first, second);
+    public static boolean eq(String left, String right)  {
+        Map<Character, Integer> first = new HashMap<>();
+        Map<Character, Integer> second = new HashMap<>();
+        char[] a = left.toCharArray();
+        char[] b = right.toCharArray();
+        put(left, first, a);
+        put(right, second, b);
+        return Objects.equals(first, second);
+    }
+
+    private static void put(String word, Map<Character, Integer> map, char[] a) {
+        for (int i = 0; i < word.length(); i++) {
+            if (map.containsKey(a[i])) {
+                int temp = map.get(a[i]);
+                map.replace(a[i], temp + 1);
+            } else {
+                map.put(a[i], 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(eq("Privet", "rPivet"));
     }
 }
